c6b184ce167e2ed82f0a3b2f7f9e650bdc838dba
Henrique <165193930+HenriqueZkx@users.noreply.github.com>
2024-06-07 17:21:52 -0300

    Update PessoaViewModel.kt

--- AppAula/AppAula/app/src/main/java/com/example/appaula/viewModel/PessoaViewModel.kt
+++ AppAula/AppAula/app/src/main/java/com/example/appaula/viewModel/PessoaViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.appaula.roomdb.Pessoa
 import kotlinx.coroutines.launch
 
+/*cria a função que será executada na tabela*/
 class PessoaViewModel(private val repository: Repository): ViewModel() {
     fun getPessoa() = repository.getAllPessoa().asLiveData(viewModelScope.coroutineContext)
 
